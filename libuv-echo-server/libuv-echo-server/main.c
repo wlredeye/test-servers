06b7fb51363058896feb06c8e37e56f369e04cb0
wlredeye <dkorolko@gmail.com>
2016-06-17 15:21:46 +0300

    bug with uv_buf_t fixed

--- libuv-echo-server/libuv-echo-server/main.c
+++ libuv-echo-server/libuv-echo-server/main.c
@@ -5,23 +5,39 @@
 uv_tcp_t server;
 uv_loop_t *loop;
 
-void read_cb(uv_stream_t *stream, ssize_t nread, uv_buf_t buf){
+void read_cb(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf){
     uv_write_t *req = (uv_write_t*)malloc(sizeof(uv_write_t));
-    uv_write(req, stream, &buf, 1, NULL);
-    free(buf.base);
+    
+    uv_buf_t wbuf = uv_buf_init(buf->base, nread);
+    int r = uv_write(req, stream, &wbuf, 1, NULL);
+    
+    if (r < 0)
+        fprintf(stderr, "Error on writing client stream: %s.\n", r);
+    
+    free(wbuf.base);
+    free(req);
 }
 
-uv_buf_t alloc_buffer(uv_handle_t *handle, size_t size){
-    return uv_buf_init((char*)malloc(size), size);
+void alloc_buffer(uv_handle_t * handle, size_t size, uv_buf_t *buf) {
+    buf->base = (char*)malloc(size);
+    buf->len = size;
 }
 
 void connection_cb(uv_stream_t *server,int status){
     uv_tcp_t *client = malloc(sizeof(uv_tcp_t));
     
+    if (status < 0)
+        fprintf(stderr, "Error on listening: %s.\n", status);
+    
     uv_tcp_init(loop, client);
-    uv_accept(server, (uv_stream_t*)client);
-    uv_read_start((uv_stream_t*)client, alloc_buffer, read_cb);
+    if (uv_accept(server, (uv_stream_t *) client) == 0) {
+        int r = uv_read_start((uv_stream_t *) client, alloc_buffer, read_cb);
+        if (r < 0)
+            fprintf(stderr, "Error on reading client stream: %s.\n", r);
+    } else
+        uv_close((uv_handle_t *) client, NULL); /* close client stream on error */
 }
+
 int main(int argc, const char * argv[]) {
     loop = uv_default_loop();
     
@@ -30,7 +46,8 @@ int main(int argc, const char * argv[]) {
     
     uv_tcp_init(loop, &server);
     uv_tcp_bind(&server, &addr, 0);
-    uv_listen((uv_stream_t*)&server, 128, connection_cb);
-    
-    return rv_run(loop, UV_RUN_DEFAULT);
+    int r = uv_listen((uv_stream_t*)&server, 128, connection_cb);
+    if (r < 0 )
+        return fprintf(stderr, "Error on listening: %s.\n", r);
+    return uv_run(loop, UV_RUN_DEFAULT);
 }
